e19349778f5fc4fc6b2355daa8d9f7bd25e85748
krutika thakkar <thakkarkrutika4@gmail.com>
2024-09-27 16:50:40 +0530

    added client

--- server/database.js
+++ server/database.js
@@ -9,21 +9,4 @@ const pool = new Pool({
     database: "visitor_info"
 });
 
-const createtblquery = `CREATE TABLE IPRScientificVisitForm (
-    id SERIAL PRIMARY KEY,
-    campus TEXT[],
-    ipr_time VARCHAR(50),
-    fcipt_time VARCHAR(50),
-    visit_date DATE,
-    visit_time TIME,
-    materials TEXT[],
-    email VARCHAR(100)
-);`
-
-pool.query (createtblquery).then((response)=>{
-    console.log(response);
-}).catch((err)=>{
-    console.log(err);
-});
-
 module.exports = pool;
